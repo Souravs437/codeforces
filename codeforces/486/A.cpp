c4e6a7e552398ee6b2d94cd102aa7e2c6ac968a8
souravkumarsingh9897@gmail.com <souravkumarsingh9897@gmail.com>
2018-11-16 13:44:00 +0530

    Add solution for problem `A - Calculating Function` Link: https://codeforces.com/contest/486/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/486/submission/45792534

---
+++ codeforces/486/A.cpp
@@ -0,0 +1,29 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+
+const int N = 2 * 1e5 + 10;
+
+ll res;
+ll a[N];
+
+
+void solve(){
+    ll n ;
+    cin>>n;
+    if(n%2 == 0)
+         cout<<n/2;
+    else 
+        cout<<(n/2-n);
+}
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+    ll t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
