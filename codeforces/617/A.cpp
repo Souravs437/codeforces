a8a239a08e02bfab073e990396469a9906ddb7b5
souravkumarsingh9897@gmail.com <souravkumarsingh9897@gmail.com>
2018-11-16 13:58:00 +0530

    Add solution for problem `A - Elephant` Link: https://codeforces.com/contest/617/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/617/submission/45792887

---
+++ codeforces/617/A.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+
+const int N = 2 * 1e5 + 10;
+
+ll res;
+ll a[N];
+
+
+void solve(){
+    ll n ,count;
+    cin>>n;
+    if(n%5==0)
+        count = n/5;
+    else
+        count= n/5 + 1;
+    cout<<count;
+}
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+    ll t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
