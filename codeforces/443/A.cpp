cf29d0e7ef4811335d566d6944f860ea6d9136da
souravkumarsingh9897@gmail.com <souravkumarsingh9897@gmail.com>
2018-11-18 04:53:00 +0530

    Add solution for problem `A - Anton and Letters` Link: https://codeforces.com/contest/443/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/443/submission/45896710

---
+++ codeforces/443/A.cpp
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+
+const int N = 2 * 1e5 + 10;
+
+ll res;
+ll a[N];
+
+
+void solve(){
+    set <char> str;
+    char ch;
+    while(cin >> ch)
+    {
+        if(isalpha(ch))
+            str.insert(ch);
+    }
+    cout<<str.size();
+}
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+    ll t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
