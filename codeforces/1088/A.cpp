a9bed909612e9900be20ae09e8e6f9a50a5a6e34
souravkumarsingh9897@gmail.com <souravkumarsingh9897@gmail.com>
2018-12-15 00:58:00 +0530

    Add solution for problem `A - Ehab and another construction problem` Link: https://codeforces.com/contest/1088/problem/A Tags: brute force, constructive algorithms, *800 Ref: https://codeforces.com/contest/1088/submission/47016653

---
+++ codeforces/1088/A.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+
+const int N = 2 * 1e5 + 10;
+
+ll res;
+ll a[N];
+
+
+void solve(){
+    ll n;
+    cin>>n;
+    if(n < 2) { cout<<"-1"; }
+    else
+    {
+        cout<<n<<" "<<n;
+    }
+}
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+    ll t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
