23bc99d50459c29b73715f9c59262ab14e3994c9
souravkumarsingh9897@gmail.com <souravkumarsingh9897@gmail.com>
2018-12-21 16:07:00 +0530

    Add solution for problem `B - Buttons` Link: https://codeforces.com/contest/268/problem/B Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/268/submission/47326353

---
+++ codeforces/268/B.cpp
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+
+const int N = 2 * 1e5 + 10;
+
+ll res;
+ll a[N];
+
+
+void solve(){
+    ll n , sum = 0;
+    cin>>n;
+    sum = n;
+    for (int i = 1; i <= n; ++i)
+    {
+        sum += (n - i) * i;
+    }
+    cout<<sum;
+}
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+    ll t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
