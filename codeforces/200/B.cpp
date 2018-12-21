d04b81d02d83acf72dda99b4457003bbb37c5227
souravkumarsingh9897@gmail.com <souravkumarsingh9897@gmail.com>
2018-12-21 12:16:00 +0530

    Add solution for problem `B - Drinks` Link: https://codeforces.com/contest/200/problem/B Tags: implementation, math, *800 Ref: https://codeforces.com/contest/200/submission/47319515

---
+++ codeforces/200/B.cpp
@@ -0,0 +1,38 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+
+const int N = 2 * 1e5 + 10;
+
+ll res;
+ll a[N];
+
+
+void solve(){
+    ll n;
+    double a[N];
+    double sum = 0;
+    cin>>n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin>>a[i];
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        sum += (double)(a[i]/100);
+    }
+    sum /= n;
+    sum *= 100;
+    cout<<fixed<<setprecision(7)<<sum;
+}
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+    ll t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
