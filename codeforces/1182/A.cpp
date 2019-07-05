a9ae84ff2c0dd323a022c64fc15e96a55458ed09
souravkumarsingh9897@gmail.com <souravkumarsingh9897@gmail.com>
2019-07-05 08:06:00 +0530

    Add solution for problem `A - Filling Shapes` Link: https://codeforces.com/contest/1182/problem/A Tags: dp, math, *1000 Ref: https://codeforces.com/contest/1182/submission/56537849

---
+++ codeforces/1182/A.cpp
@@ -0,0 +1,31 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+
+const int N = 2 * 1e5 + 10;
+
+ll res;
+ll a[N];
+
+
+void solve(){
+    ll n;
+    cin>>n;
+    if(n % 2 == 0){
+        ll ans = pow(2 , n /2);
+        cout<<ans<<endl;
+    } 
+    else
+        cout<<0<<endl;
+}
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+    ll t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
