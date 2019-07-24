291a22c5ef60a916635a391fd4890e2333c92d0f
souravkumarsingh9897@gmail.com <souravkumarsingh9897@gmail.com>
2019-07-24 17:56:00 +0530

    Add solution for problem `B - Equalize Prices` Link: https://codeforces.com/contest/1183/problem/B Tags: math, *900 Ref: https://codeforces.com/contest/1183/submission/57640466

---
+++ codeforces/1183/B.cpp
@@ -0,0 +1,39 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+
+const int N = 2 * 1e5 + 10;
+
+ll res;
+ll a[N];
+
+
+void solve(){
+    ll n, k, min, max;
+    cin>>n>>k;
+    for (int i = 0; i < n; ++i)
+    {
+        cin>>a[i];
+    }
+
+    sort( a , a+ n);
+
+    min = a[0];
+    max = a[n -1];
+    if(min + k >= max - k) {
+        cout<<min+k<<"\n";
+    }
+    else
+        cout<<-1<<"\n";
+}
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+    ll t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
