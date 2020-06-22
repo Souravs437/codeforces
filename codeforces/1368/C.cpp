fb64bc1101e974686641ddb44239cb844e2fc577
souravkumarsingh9897@gmail.com <souravkumarsingh9897@gmail.com>
2020-06-22 11:10:00 +0530

    Add solution for problem `C - Even Picture` Link: https://codeforces.com/contest/1368/problem/C Tags: constructive algorithms, *1500 Ref: https://codeforces.com/contest/1368/submission/84632770

---
+++ codeforces/1368/C.cpp
@@ -0,0 +1,65 @@
+//SkGeN
+#include<iostream>
+#include<algorithm>
+#include<bitset>
+#include<cmath>
+#include<cstring>
+#include<climits>
+#include<deque>
+#include<queue>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_set>
+#include<unordered_map>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef pair<ll,ll> p64;
+typedef vector<ll> v64;
+const int N = 2 * 1e5 + 10;
+const ll inf = 1e18 + 100;
+const ll mod = 1e9 + 7;
+ 
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define rep(i,s,e) for(long long i=s;i<=e;i++)
+#define brep(i,s,e) for(long long i=s;i>=e;i--)
+#define all(x) x.begin(),x.end()
+#define mem(x,y) memset(x,y,sizeof(x))
+#define DANGER std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+void solve() {
+	ll n;
+	cin>>n;
+	vector<pair<ll,ll>> v;
+	for(int i = 0; i <= n+1; i++) {
+		v.pb(mp(i,i));
+	}
+
+	for(int i = 0 ; i <= n ; i++) {
+		v.pb(mp(i,i+1));
+		v.pb(mp(i+1,i));	
+	}
+
+	cout<<v.size()<<"\n";
+	for(auto i : v) {
+		cout<<i.first<<" "<<i.second<<"\n";
+	}
+
+
+}
+int main()
+{
+	DANGER;
+	ll t = 1;
+	//cin>>t;
+	while(t--) {
+		solve();
+	}
+return 0;
+}
+//---->O ,,
