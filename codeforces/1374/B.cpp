cf8db9b46b2e5afcabf9eef9525cb29722ee90af
souravkumarsingh9897@gmail.com <souravkumarsingh9897@gmail.com>
2020-06-28 20:15:00 +0530

    Add solution for problem `B - Multiply by 2, divide by 6` Link: https://codeforces.com/contest/1374/problem/B Tags: math, *900 Ref: https://codeforces.com/contest/1374/submission/85306180

---
+++ codeforces/1374/B.cpp
@@ -0,0 +1,67 @@
+//SkGeN
+#include<iostream>
+#include<algorithm>
+#include<bitset>
+#include<cmath>
+#include<cstring>
+#include<climits>
+#include<deque>
+#include<queue>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_set>
+#include<unordered_map>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef pair<ll,ll> p64;
+typedef vector<ll> v64;
+const int N = 2 * 1e5 + 10;
+const ll inf = 1e18 + 100;
+const ll mod = 1e9 + 7;
+ 
+#define pb push_back
+#define ff first
+#define ss second
+#define rep(i,s,e) for(long long i=s;i<=e;i++)
+#define brep(i,s,e) for(long long i=s;i>=e;i--)
+#define all(x) x.begin(),x.end()
+#define mem(x,y) memset(x,y,sizeof(x))
+#define DANGER std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+
+
+void solve() {
+	ll n ;
+	cin>>n;
+	map<ll,ll> mp;
+	ll ans = 0;
+	while(mp.count(n) == 0 && n != 1) {
+		mp[n]++;
+		if(n % 6 == 0) {
+			n /= 6;
+		} else {
+			n *= 2;
+		}
+		ans++;
+	}
+	//cout<<n<<" ";
+	if(n == 1) {
+		cout<<ans<<"\n";
+	} else {
+		cout<<"-1\n";
+	}
+}
+int main()
+{
+	DANGER;
+	ll t = 1;
+	cin>>t;
+	while(t--) {
+		solve();
+	}
+return 0;
+}
+//---->O ,,
