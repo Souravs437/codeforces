c3ae6b5b669f74ba4243abfb7e6faa6f280f9257
souravkumarsingh9897@gmail.com <souravkumarsingh9897@gmail.com>
2020-08-25 20:18:00 +0530

    Add solution for problem `A - String Similarity` Link: https://codeforces.com/contest/1400/problem/A Tags: constructive algorithms, strings, *800 Ref: https://codeforces.com/contest/1400/submission/90921363

---
+++ codeforces/1400/A.cpp
@@ -0,0 +1,39 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define S second
+#define F first
+#define all(a) a.begin() , a.end()
+#define pb push_back 
+#define DBG cout<<"debug\n";
+
+typedef long long ll;
+
+const int N = 2 * 1e5 + 10;
+const ll inf = 9e18 + 9; 
+
+ll res;
+ll a[N];
+
+
+void solve(){
+  ll n;
+  cin>>n;
+  string s;
+  cin>>s;
+  string ans = "";
+  for(int i = 1; i <= n; i++) {
+    ans += s[2*i - 2];
+  }
+  cout<<ans<<"\n";
+}
+
+int main(int argc, char const *argv[]){
+  ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+  ll t = 1;
+  cin >> t;
+  while(t--){
+    solve();
+  }
+  return 0;
+}
