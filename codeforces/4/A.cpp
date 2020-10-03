210a62481da2ac56e5a7e08a4930f415e42c2a38
souravkumarsingh9897@gmail.com <souravkumarsingh9897@gmail.com>
2020-10-03 17:41:00 +0530

    Add solution for problem `A - Watermelon` Link: https://codeforces.com/contest/4/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/4/submission/94568769

---
+++ codeforces/4/A.cpp
@@ -0,0 +1,37 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define S second
+#define F first
+#define all(a) a.begin() , a.end()
+#define pb push_back 
+#define DBG cout<<"debug\n";
+
+typedef long long ll;
+
+const int N = 2 * 1e5 + 10;
+const ll inf = 9e18 + 9; 
+
+ll res;
+ll a[N];
+
+
+void solve(){
+  ll n;
+  cin >> n;
+  if(n % 2 || n == 2) {
+    cout<<"NO\n";
+  } else {
+    cout<<"YES\n";
+  }
+}
+
+int main(int argc, char const *argv[]){
+  ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+  ll t = 1;
+  //cin >> t;
+  while(t--){
+    solve();
+  }
+  return 0;
+}
