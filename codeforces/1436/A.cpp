532b67cd976661e28b418459df80bf7b6ef0a757
souravkumarsingh9897@gmail.com <souravkumarsingh9897@gmail.com>
2020-10-24 19:40:00 +0530

    Add solution for problem `A - Reorder` Link: https://codeforces.com/contest/1436/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1436/submission/96538568

---
+++ codeforces/1436/A.cpp
@@ -0,0 +1,42 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define S second
+#define F first
+#define all(a) a.begin() , a.end()
+#define pb push_back 
+#define DBG cout<<"debug\n";
+
+typedef long long ll;
+
+const int N = 2 * 1e5 + 10;
+const ll inf = 9e18 + 9; 
+
+ll res;
+ll a[N];
+
+
+void solve(){
+  ll n , m;
+  cin >> n >> m;
+  ll sum = 0;
+  for(int i = 0; i < n ; i++) {
+    ll xx ; cin >> xx;
+    sum += xx;
+  }
+  if(sum == m) {
+    cout<<"YES\n";
+  } else {
+    cout<<"NO\n";
+  }
+}
+
+int main(int argc, char const *argv[]){
+  ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+  ll t = 1;
+  cin >> t;
+  while(t--){
+    solve();
+  }
+  return 0;
+}
